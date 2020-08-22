023d31d92e4862aa23572616c3d91ff097110967
Gillian Liang <Veladaraston@Gillians-MacBook-Pro.local>
2020-08-21 21:22:58 -0400

    singer's bio page complete

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+function myBio() {
+    var x = document.getElementById("myDIY");
+    
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
